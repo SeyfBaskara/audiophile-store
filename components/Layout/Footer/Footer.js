5290da3535b643373dbcf30bd5f7cfcdcf7b7a27
Seyf <seyfettin.baskara@gmail.com>
2022-10-19 13:48:33 +0200

    add full responsive css to footer section

--- components/Layout/Footer/Footer.js
+++ components/Layout/Footer/Footer.js
@@ -6,20 +6,20 @@ const Footer = ({ footer }) => {
    const { navigation, description, copyright } = footer
 
    const customStyle = {
-      navBar: 'flex flex-col gap-5',
+      navBar: 'flex flex-col gap-5 sm:flex-row',
    }
 
    return (
-      <footer className="relative bg-sectionBlack pt-4 pb-16">
-         <section className="text-center m-auto space-y-11 px-7">
-            <div className="bg-peruOrange h-1 w-28 absolute top-0 left-32"></div>
-            <div className="space-y-10">
+      <footer className="bg-sectionBlack pt-4 pb-16">
+         <section className="text-center m-auto space-y-11 px-7 sm:text-left lg:px-20">
+            <div className="bg-peruOrange m-auto -mt-4 h-1 w-28 sm:ml-0"></div>
+            <div className="space-y-10 lg:flex lg:justify-between lg:items-center lg:space-y-0">
                <Image src="/images/logo.svg" alt="website logo" width={143} height={25} />
                <Nav navigation={navigation} customStyle={customStyle.navBar} />
             </div>
-            <p className="text-spanishGray text-sm">{description}</p>
+            <p className="text-spanishGray text-sm lg:w-96">{description}</p>
 
-            <div className="space-y-10">
+            <div className="flex flex-col gap-10 sm:flex-row sm:justify-between sm:items-center sm:gap-0">
                <p className="text-spanishGray">{copyright}</p>
                <div className="flex justify-center gap-3">
                   {['facebook', 'twitter', 'instagram'].map((path, index) => (
--- components/Layout/Header/ThumbnailHeader.js
+++ components/Layout/Header/ThumbnailHeader.js
@@ -13,7 +13,8 @@ const ThumbnailHeader = ({ thumbnail }) => {
       <section className="relative">
          <div
             className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-                        flex flex-col items-center text-center gap-y-5 h-64 w-80 z-10 sm:w-96 md:w-80 md:left-56 md:items-start md:text-left lg:w-96 lg:left-72"
+                        flex flex-col items-center text-center gap-y-5 h-64 w-80 z-10 sm:w-96 
+                        md:w-80 md:left-56 md:items-start md:text-left lg:w-96 lg:left-72"
          >
             <p className="text-spanishGray tracking-[.55em]">NEW PRODUCT</p>
             <h1 className="text-white text-xl4 font-semibold leading-none sm:text-xl5 md:text-xl4 lg:text-xl5">
