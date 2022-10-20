b91bc6bada4d14776767538bd223570088391b9f
Seyf <seyfettin.baskara@gmail.com>
2022-10-20 20:38:12 +0200

    add widgets on headphones page

--- pages/category/headphones.js
+++ pages/category/headphones.js
@@ -1,4 +1,6 @@
 import Layout from '../../components/Layout'
+import MenuWidget from '../../components/Widgets/MenuWidget'
+import PosterCardWidget from '../../components/Widgets/PosterCardWidget'
 import contentfulClient from '../../utils/contentfulClient'
 
 export async function getStaticProps() {
@@ -9,22 +11,37 @@ export async function getStaticProps() {
       content_type: 'footer',
    })
 
-   const [header, footer] = await Promise.all([headerFetch, footerFetch])
+   const menuWidgetFetch = contentfulClient.getEntries({
+      content_type: 'menuWidget',
+   })
+   const sharedWidgetFetch = contentfulClient.getEntries({
+      content_type: 'sharedWidget',
+   })
+
+   const [header, footer, menuWidgetProduct, posterCard] = await Promise.all([
+      headerFetch,
+      footerFetch,
+      menuWidgetFetch,
+      sharedWidgetFetch,
+   ])
 
    return {
       props: {
          header: header.items[0].fields,
          footer: footer.items[0].fields,
+         menuWidgetProduct: menuWidgetProduct.items,
+         posterCard: posterCard.items[0].fields,
       },
    }
 }
 
-const Headphones = ({ header, footer }) => {
+const Headphones = ({ header, footer, menuWidgetProduct, posterCard }) => {
    const { navigation } = header
 
    return (
       <Layout header={header} footer={footer} headerName={navigation[1].name}>
-         <p>Headphone page</p>
+         <MenuWidget menuWidgetProduct={menuWidgetProduct} />
+         <PosterCardWidget posterCard={posterCard} />
       </Layout>
    )
 }
