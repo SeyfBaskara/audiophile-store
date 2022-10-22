ef00028a516382c9073bf419172c304bb431a30b
Seyf <seyfettin.baskara@gmail.com>
2022-10-22 11:30:49 +0200

    add earphones page content

--- pages/category/earphones.js
+++ pages/category/earphones.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import Layout from '../../components/Layout'
+import MenuWidget from '../../components/Widgets/MenuWidget'
+import PosterCardWidget from '../../components/Widgets/PosterCardWidget'
+import contentfulClient from '../../utils/contentfulClient'
+import ProductDisplay from '../../components/Products/index'
 
-const Earphones = () => {
+export async function getStaticProps() {
+   const headerFetch = contentfulClient.getEntries({
+      content_type: 'header',
+   })
+   const footerFetch = contentfulClient.getEntries({
+      content_type: 'footer',
+   })
+   const menuWidgetFetch = contentfulClient.getEntries({
+      content_type: 'menuWidget',
+   })
+   const sharedWidgetFetch = contentfulClient.getEntries({
+      content_type: 'sharedWidget',
+   })
+   const earphonesProductFetch = contentfulClient.getEntries({
+      content_type: 'earphonesProduct',
+   })
+
+   const [header, footer, menuWidgetProduct, posterCard, earphonesProduct] = await Promise.all([
+      headerFetch,
+      footerFetch,
+      menuWidgetFetch,
+      sharedWidgetFetch,
+      earphonesProductFetch,
+   ])
+
+   return {
+      props: {
+         header: header.items[0].fields,
+         footer: footer.items[0].fields,
+         menuWidgetProduct: menuWidgetProduct.items,
+         posterCard: posterCard.items[0].fields,
+         earphonesProduct: earphonesProduct.items,
+      },
+   }
+}
+
+const Earphones = ({ header, footer, menuWidgetProduct, posterCard, earphonesProduct }) => {
+   const { navigation } = header
+
    return (
-      <div>
-         <p>Earphones page</p>
-      </div>
+      <Layout header={header} footer={footer} headerName={navigation[3].name}>
+         <ProductDisplay products={earphonesProduct} />
+         <MenuWidget menuWidgetProduct={menuWidgetProduct} />
+         <PosterCardWidget posterCard={posterCard} />
+      </Layout>
    )
 }
 
