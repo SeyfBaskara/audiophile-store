fc57469b53a5d083f9df4cb343072c5093c55e3e
Seyf <seyfettin.baskara@gmail.com>
2022-11-09 11:30:50 +0100

    add shopping cart context

---
+++ context/ShoppingCartContext.js
@@ -0,0 +1,92 @@
+import React, { useState, createContext, useContext } from 'react'
+
+const initicalContext = []
+
+const ShoppingCartContext = createContext(initicalContext)
+
+const ShoppingCartProvider = ({ children }) => {
+   const [cartItems, setCartItems] = useState(initicalContext)
+
+   const getItemQuantity = (productName) => {
+      return cartItems.find((item) => item.productName === productName)?.quantity || 0
+   }
+   const addItemToCart = (productName, quantity) => {
+      setCartItems((currItems) => {
+         if (currItems.find((item) => item.productName === productName) == null) {
+            return [...currItems, { productName, quantity }]
+         } else {
+            return currItems.map((item) => {
+               if (item.productName === productName) {
+                  return { ...item, quantity }
+               } else {
+                  return item
+               }
+            })
+         }
+      })
+   }
+   const increaseCartQuantity = (productName) => {
+      setCartItems((currItems) => {
+         if (currItems.find((item) => item.productName === productName) == null) {
+            return [...currItems, { productName, quantity: 1 }]
+         } else {
+            return currItems.map((item) => {
+               if (item.productName === productName) {
+                  return { ...item, quantity: item.quantity + 1 }
+               } else {
+                  return item
+               }
+            })
+         }
+      })
+   }
+   const decreaseCartQuantity = (productName) => {
+      setCartItems((currItems) => {
+         if (currItems.find((item) => item.productName === productName)?.quantity === 1) {
+            return cartItems.filter((item) => item.productName !== productName)
+         } else {
+            return currItems.map((item) => {
+               if (item.productName === productName) {
+                  return { ...item, quantity: item.quantity - 1 }
+               } else {
+                  return item
+               }
+            })
+         }
+      })
+   }
+   const removeFromCart = (productName) => {
+      setCartItems((currItems) => {
+         return currItems.filter((item) => item.productName !== productName)
+      })
+   }
+   const removeAllFromCart = () => {
+      setCartItems([])
+   }
+
+   const value = {
+      cartItems,
+      getItemQuantity,
+      addItemToCart,
+      increaseCartQuantity,
+      decreaseCartQuantity,
+      removeFromCart,
+      removeAllFromCart,
+   }
+   return (
+      <>
+         <ShoppingCartContext.Provider value={value}>{children}</ShoppingCartContext.Provider>
+      </>
+   )
+}
+
+export const useShoppingContext = () => useContext(ShoppingCartContext)
+export default ShoppingCartProvider
+
+/**
+ * get cart item's quantity
+ * increase item quantity in cart
+ * decrease item quactity in cart
+ * remove item in cart
+ * remove all items in cart
+ */
