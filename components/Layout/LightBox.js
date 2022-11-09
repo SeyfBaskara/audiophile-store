a48d62d0ef2b8db2365a6ed6de636f7158cb0a9a
Seyf <seyfettin.baskara@gmail.com>
2022-11-09 17:30:29 +0100

    fix lightbox overlap on scrolling

--- components/Layout/LightBox.js
+++ components/Layout/LightBox.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { useThemeContext } from '../../context/ThemeContext'
+import { useShoppingContext } from '../../context/ShoppingCartContext'
 
 const LightBox = () => {
    const [scrollY, setScrollY] = useState(0)
    const { showLightBox } = useThemeContext()
+   const { isCartOpen } = useShoppingContext()
 
    useEffect(() => {
       const handleScroll = () => {
@@ -23,7 +25,10 @@ const LightBox = () => {
       <>
          {showLightBox && (
             <div
-               className={`fixed w-full h-full left-0 ${scrollY >= 100 ? 'top-0' : 'top-24'} bg-black opacity-30 z-40`}
+               className={`fixed w-full h-full left-0 ${
+                  isCartOpen && scrollY >= 13 ? 'top-0' : scrollY >= 100 ? 'top-0' : 'top-20'
+               }
+               bg-black opacity-30 z-40`}
             ></div>
          )}
       </>
@@ -31,3 +36,7 @@ const LightBox = () => {
 }
 
 export default LightBox
+
+/**
+ * should fix lightbox overlap icons on scrolling
+ */
