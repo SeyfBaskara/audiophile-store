3c2a0c9b4d62cfc3ed23abd314946c22d697e6e0
Seyf <seyfettin.baskara@gmail.com>
2022-11-11 14:39:19 +0100

    add payment details input

--- components/Checkout/Forms/Form.js
+++ components/Checkout/Forms/Form.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import BillingDetailsInputs from './BillingDetailsInputs'
+import PaymentDetailsInputs from './PaymentDetailsInputs'
 import ShippingInfoInputs from './ShippingInfoInputs'
 
 const customStyle = {
    title: 'text-sm text-peruOrange font-semibold tracking-wider mb-3',
-   label: 'flex flex-col gap-1 text-sm font-semibold',
+   label: 'flex flex-col gap-2 text-sm font-semibold',
    input: 'border-2 rounded-lg py-3 px-5 text-spanishGray text-[1.1rem] font-normal',
 }
 
@@ -13,15 +14,19 @@ const Form = () => {
    const {
       register,
       handleSubmit,
-      watch,
+      reset,
       formState: { errors },
    } = useForm()
-   const onSubmit = (data) => console.log(data)
+   const onSubmit = (data) => {
+      console.log(data)
+      reset()
+   }
 
    return (
       <form onSubmit={handleSubmit(onSubmit)}>
          <BillingDetailsInputs register={register} style={customStyle} />
          <ShippingInfoInputs register={register} style={customStyle} />
+         <PaymentDetailsInputs register={register} style={customStyle} />
          <input type="submit" />
       </form>
    )
---
+++ components/Checkout/Forms/PaymentDetailsInputs.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const PaymentDetailsInputs = ({ register, style }) => {
+   return (
+      <div className="my-6">
+         <h1 className={`${style.title}`}>PAYMENT DETAILS</h1>
+
+         <div className="flex flex-col gap-4">
+            <label className={`${style.label}`} htmlFor="payment method">
+               Payment Method
+               <div className={`${style.input}`}>
+                  <label className="flex items-center gap-5 font-semibold text-black text-[1rem]" htmlFor="e-Money">
+                     <input
+                        type="radio"
+                        value="e-money"
+                        {...register('paymentMethod', {
+                           required: true,
+                        })}
+                     />
+                     e-Money
+                  </label>
+               </div>
+               <div className={`${style.input} mt-2`}>
+                  <label
+                     className="flex items-center gap-5 font-semibold text-black text-[1rem]"
+                     htmlFor="cash-on-delivery"
+                  >
+                     <input
+                        type="radio"
+                        value="cash-on-delivery"
+                        {...register('paymentMethod', {
+                           required: true,
+                        })}
+                     />
+                     Cash on Delivery
+                  </label>
+               </div>
+            </label>
+
+            <label className={`${style.label}`} htmlFor="e-Money-number">
+               e-Money Number
+               <input
+                  className={`${style.input}`}
+                  type="number"
+                  {...register('eMoneyNumber', {
+                     required: true,
+                  })}
+               />
+            </label>
+
+            <label className={`${style.label}`} htmlFor="e-Money-pin">
+               e-Money PIN
+               <input
+                  className={`${style.input}`}
+                  type="number"
+                  {...register('eMoneyPin', {
+                     required: true,
+                  })}
+               />
+            </label>
+         </div>
+      </div>
+   )
+}
+
+export default PaymentDetailsInputs
